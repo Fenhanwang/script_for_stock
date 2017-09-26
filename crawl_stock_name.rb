3bafabcd803ffd2b01b1d530e716387d1a75fc38
Jack Wang <wfenhan@gmail.com>
2017-09-25 22:23:27 -0400

    commit before install Ubuntu

--- crawl_stock_name.rb
+++ crawl_stock_name.rb
@@ -4,18 +4,48 @@ require 'elasticsearch'
 require 'json'
 require 'pp'
 require 'csv'
+require 'pry-byebug'
 
 class CrawlStockName
 
-  def initialize
-    @url = "http://eoddata.com/stocklist/NYSE/%s.htm"
+  def initialize(zhang_fu)
+    @url = "http://eoddata.com/stocklist/NASDAQ/%s.htm"
     @char_array = ("A".."Z").to_a
     @stock_abb_array = []
+    @zhang_fu = zhang_fu # 0.3
   end
 
-  def crawl
+  def min_in_52_week
     crawl_stock_name
     loop_stock_array
+  end
+
+  def biao_zhun_cha
+  	i = 1
+    crawl_stock_name
+    
+    @stock_abb_array.each_slice(20) do |arr|
+    	threads = []
+	    arr.each { |e|
+	      # cal_stock_trend(e, 30, "Close")
+	      # cal_stock_trend(e, 30, "Volume", 100)
+	      # cal_x_day_min_point(e, 30, "Close")
+	      puts i if i%1000 == 0  
+	      threads << Thread.new(e){|ee|cal_protential_zuokong(ee, 30, @zhang_fu)}
+	      # threads << Thread.new(e){|ee| penny_stock(ee, 30, @zhang_fu)}
+	      i+=1
+	    }
+	    # sleep 5
+	    threads.each(&:join)
+	end
+	# @stock_abb_array.each { |e|
+	# 	cal_protential_zuokong(e, 30, 0.3)
+	# 	puts i
+	# 	i+=1
+	# 	sleep 1 if i % 15 == 0
+	# 	sleep 0.5
+	# }
+
   end
 
   def crawl_stock_name
@@ -24,11 +54,11 @@ class CrawlStockName
       page = Nokogiri::HTML(open(current_url))
       page.xpath("//div[@id='ctl00_cph1_divSymbols']/table/tr")[1..-1].each { |tr|
       tds = tr.elements
-      # puts tds.first.content
       @stock_abb_array << tds.first.content
       }
     }
-    puts @stock_abb_array.size
+
+    puts "Stock Size is #{@stock_abb_array.size}"
   end
 
   def loop_stock_array
@@ -54,9 +84,9 @@ class CrawlStockName
         change_percent = ( ( ( current_value - low_fiftytwo ) / range_between ) * 100 ).to_i
         puts line
         puts ""
-        if change_percent < 50 && line[10] =~ /B/ && range_between > 20 && low_fiftytwo < 100
+        if change_percent < 50 && line[10] =~ /B|M/ && range_between > 20 && low_fiftytwo < 100
           puts line.join("---")
-          puts change_percent
+          puts "Change Percent: #{change_percent}%"
           puts ""
         end
       elsif line[5] != 'N/A' && line[7] != 'N/A'
@@ -68,13 +98,173 @@ class CrawlStockName
 
         # if the percent is too high, which means market value is too high
         change_percent = ( ( ( current_value - low_fiftytwo ) / range_between ) * 100 ).to_i
-        if change_percent < 50 && line[10] =~ /B/ && range_between > 20 && low_fiftytwo < 100
+        if change_percent < 50 && line[10] =~ /B|M/ && range_between > 20 && low_fiftytwo < 100
           puts line.join("---")
-          puts change_percent
+          puts "Change Percent: #{change_percent}%"
           puts ""
         end
       end
     end
+  end
+
+  def cal_stock_trend(stock_name, days, type, var_bar)
+    yesterday_format  = ( Time.now - 1 * 3600 * 24 ).strftime("%Y-%m-%d")
+    many_days_ago = ( Time.now - days * 3600 * 24 ).strftime("%Y-%m-%d")
+    stock_name = stock_name.split("-").first
+    # https://developer.yahoo.com/yql/console/?q=show%20tables&env=store://datatables.org/alltableswithkeys#h=select+*+from+yahoo.finance.historicaldata+where+symbol+%3D+%22YHOO%22+and+startDate+%3D+%222009-09-11%22+and+endDate+%3D+%222010-03-10%22
+    res = open("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.historicaldata%20where%20symbol%20%3D%20%22#{stock_name}%22%20and%20startDate%20%3D%20%22#{many_days_ago}%22%20and%20endDate%20%3D%20%22#{yesterday_format}%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=").read
+    result_json = JSON.parse(res)
+    return false if result_json['query']['results'].nil?
+    begin
+      input_array = result_json['query']['results']['quote'].map { |e| Float(e["#{type}"]) }
+    rescue Exception => e
+      puts "Error! #{result_json}"   
+    end
+    input_array = input_array.map { |e| Integer(e/10000) } if type == "Volume"
+    var_res =  variance(input_array)
+    if var_res > var_bar
+      puts "====begin #{type}====", var_res, stock_name, "===end==="
+      puts ""
+    end
+  end
+
+  def cal_x_day_min_point(stock_name, days, type)
+    yesterday_format  = ( Time.now - 1 * 3600 * 24 ).strftime("%Y-%m-%d")
+    many_days_ago = ( Time.now - days * 3600 * 24 ).strftime("%Y-%m-%d")
+    stock_name = stock_name.split("-").first
+    # https://developer.yahoo.com/yql/console/?q=show%20tables&env=store://datatables.org/alltableswithkeys#h=select+*+from+yahoo.finance.historicaldata+where+symbol+%3D+%22YHOO%22+and+startDate+%3D+%222009-09-11%22+and+endDate+%3D+%222010-03-10%22
+    res = open("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.historicaldata%20where%20symbol%20%3D%20%22#{stock_name}%22%20and%20startDate%20%3D%20%22#{many_days_ago}%22%20and%20endDate%20%3D%20%22#{yesterday_format}%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=").read
+    result_json = JSON.parse(res)
+    return false if result_json['query']['results'].nil?
+    begin
+      input_array = result_json['query']['results']['quote'].map { |e| Float(e["#{type}"]) }
+    rescue Exception => e
+      puts "Error! #{result_json}"   
+    end
+    if input_array[0]/(input_array.min) < 0.4
+    	puts "====begin #{type}====", stock_name, "===end==="
+    	puts ""
+	end  	
+  end
+
+  def penny_stock(stock_name, days, zhang_fu)
+    # yesterday_format  = ( Time.now - 1 * 3600 * 24 ).strftime("%Y-%m-%d")
+    # many_days_ago = ( Time.now - days * 3600 * 24 ).strftime("%Y-%m-%d")
+	input_array = []
+	error_array = []
+    yesterday_format  = ( Time.now - 1 * 3600 * 24 ).strftime("%b+%d,+%Y")
+    many_days_ago = ( Time.now - days * 3600 * 24 ).strftime("%b+%d,+%Y")
+    stock_name = stock_name.split("-").first
+    # Yahoo API is down. switch google 
+    # https://stackoverflow.com/questions/11516633/how-to-work-with-google-finance
+    # res = open("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.historicaldata%20where%20symbol%20%3D%20%22#{stock_name}%22%20and%20startDate%20%3D%20%22#{many_days_ago}%22%20and%20endDate%20%3D%20%22#{yesterday_format}%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=").read
+    # result_json = JSON.parse(res)
+  	# return false if result_json['query']['results'].nil?
+
+
+    url = "http://www.google.com/finance/historical?q=NASDAQ:#{stock_name}&startdate=#{many_days_ago}&enddate=#{yesterday_format}&output=CSV"
+   	begin
+	    CSV.new(open(url)).each do |line|
+	    	close_value = line[4].to_f
+	    	next if close_value == 0
+	    	high_value  = line[2].to_f
+	    	column = line[5].to_f
+	    	input_array << { close_v: close_value, high_v: high_value, column: column }
+	    end
+   	rescue Exception => e
+		error_array << "Error-->#{stock_name}, #{url}"
+		return false  		
+   	end
+    return false if input_array.empty?
+    begin
+    max_price_within_one_month = input_array.map { |e| e[:high_v] }.max
+    index_of_the_max = input_array.map { |e| e[:high_v] }.index(max_price_within_one_month)
+    price_one_month_ago = input_array.map { |e| e[:close_v] }.last
+    price_now = input_array.map { |e| e[:close_v] }.first
+    min_colum = input_array.map { |e| e[:column] }.min
+    # zhang fu da yu 30% and max price happened within 5 days
+    if price_now < 1
+    	puts "============="
+    	puts stock_name, price_now, max_price_within_one_month, price_one_month_ago
+    	puts "============="
+    	puts ""
+    	puts ""
+  	end
+    	
+    rescue Exception => e
+		binding.pry    	
+    end
+  	
+  end
+
+  def cal_protential_zuokong(stock_name, days, zhang_fu) # zhang_fu = 0.3 means 30%
+    # yesterday_format  = ( Time.now - 1 * 3600 * 24 ).strftime("%Y-%m-%d")
+    # many_days_ago = ( Time.now - days * 3600 * 24 ).strftime("%Y-%m-%d")
+	input_array = []
+	error_array = []
+    yesterday_format  = ( Time.now - 1 * 3600 * 24 ).strftime("%b+%d,+%Y")
+    many_days_ago = ( Time.now - days * 3600 * 24 ).strftime("%b+%d,+%Y")
+    stock_name = stock_name.split("-").first
+    # Yahoo API is down. switch google 
+    # https://stackoverflow.com/questions/11516633/how-to-work-with-google-finance
+    # res = open("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.historicaldata%20where%20symbol%20%3D%20%22#{stock_name}%22%20and%20startDate%20%3D%20%22#{many_days_ago}%22%20and%20endDate%20%3D%20%22#{yesterday_format}%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=").read
+    # result_json = JSON.parse(res)
+  	# return false if result_json['query']['results'].nil?
+
+
+    url = "http://www.google.com/finance/historical?q=NASDAQ:#{stock_name}&startdate=#{many_days_ago}&enddate=#{yesterday_format}&output=CSV"
+   	begin
+	    CSV.new(open(url)).each do |line|
+	    	close_value = line[4].to_f
+	    	next if close_value == 0
+	    	high_value  = line[2].to_f
+	    	column = line[5].to_f
+	    	input_array << { close_v: close_value, high_v: high_value, column: column }
+	    end
+   	rescue Exception => e
+		error_array << "Error-->#{stock_name}, #{url}"
+		return false  		
+   	end
+    return false if input_array.empty?
+    begin
+    max_price_within_one_month = input_array.map { |e| e[:high_v] }.max
+    index_of_the_max = input_array.map { |e| e[:high_v] }.index(max_price_within_one_month)
+    price_one_month_ago = input_array.map { |e| e[:close_v] }.last
+    price_now = input_array.map { |e| e[:close_v] }.first
+    min_colum = input_array.map { |e| e[:column] }.min
+    # zhang fu da yu 30% and max price happened within 5 days
+    if ( (( max_price_within_one_month - price_one_month_ago ) / price_one_month_ago ) > zhang_fu ) and index_of_the_max < 7 and ( min_colum / 1000000 ) > 1
+    	puts "============="
+    	puts stock_name, price_now, max_price_within_one_month, price_one_month_ago
+    	puts "============="
+    	puts ""
+    	puts ""
+  	end
+    	
+    rescue Exception => e
+		binding.pry    	
+    end
+    puts error_array
+  end
+
+
+
+
+  private
+
+  # https://engineering.sharethrough.com/blog/2012/09/12/simple-linear-regression-using-ruby/
+  def mean(values)
+    total = values.reduce(0) { |sum, x| x + sum }
+    Float(total) / Float(values.length)
+  end
+
+
+  def variance(array_values)
+    x_mean = mean(array_values)
+    denominator = array_values.reduce(0) do |sum, x|
+      sum + ((x - x_mean) ** 2)
+    end
+    Math.sqrt(Float(denominator) / Float(array_values.length))
   end
 
   # def get_one_stock_data(market, stock)
@@ -86,7 +276,10 @@ class CrawlStockName
   # end
 end
 
-CrawlStockName.new.crawl
+# CrawlStockName.new.min_in_52_week
+puts "===================== >> 30% ================"
+CrawlStockName.new(0.2).biao_zhun_cha
+# CrawlStockName.new.cal_stock_trend("T", 40)
 
 
 # CrawlStockName.new.get_stockinfo_from_yahoo("http://finance.yahoo.com/d/quotes.csv?s=AAPL+GOOG+NFLX&f=sabb2b3jkj6k5vj1")
---
+++ real_time_xian_stock.rb
@@ -0,0 +1,349 @@
+require 'nokogiri'
+require 'open-uri'
+require 'json'
+require 'pp'
+require 'csv'
+require 'pry-byebug'
+require 'mongo'
+require 'headless'
+require 'selenium-webdriver'
+# require 'yahoo-finance'
+# require 'intrinio-realtime'
+# require 'eventmachine'
+
+Mongo::Logger.logger.level = ::Logger::INFO
+
+class RealTimeXianStock
+
+  def initialize(zhang_fu)
+    @url = "http://eoddata.com/stocklist/NASDAQ/%s.htm"
+    @char_array = ("A".."Z").to_a
+    @stock_abb_array = []
+    @zhang_fu = zhang_fu # 0.3
+    @count = 0
+    @client = Mongo::Client.new([ '127.0.0.1:27017' ], :database => 'real_time_stock')
+    @db = @client.database
+    # @yahoo_client = YahooFinance::Client.new
+  end
+
+  def biao_zhun_cha
+  	i = 1
+    crawl_stock_name
+    
+    @stock_abb_array.each_slice(20) do |arr|
+    	threads = []
+	    arr.each { |e|
+	      # cal_stock_trend(e, 30, "Close")
+	      # cal_stock_trend(e, 30, "Volume", 100)
+	      puts i if i%1000 == 0  
+	      threads << Thread.new(e){|ee|cal_protential_zuokong(ee, 30, @zhang_fu)}
+	      i+=1
+	    }
+	    # sleep 5
+	    threads.each(&:join)
+    end
+	end
+
+
+  def crawl_stock_name
+    @char_array.each { |e|
+      current_url = @url % [e]
+      page = Nokogiri::HTML(open(current_url))
+      page.xpath("//div[@id='ctl00_cph1_divSymbols']/table/tr")[1..-1].each { |tr|
+      tds = tr.elements
+      @stock_abb_array << tds.first.content
+      }
+    }
+
+    puts "Stock Size is #{@stock_abb_array.size}"
+  end
+
+
+  def pa_wang_ye
+    headless = Headless.new
+    headless.start
+
+    # options = Selenium::WebDriver::Chrome::Options.new
+    # Selenium::WebDriver::Chrome.driver_path="/home/fen/Documents/script_for_stock/chromedriver"
+    # options.binary = "/home/fen/Documents/script_for_stock/chromedriver" 
+    # options.add_argument('--ignore-certificate-errors')
+    # options.add_argument('--disable-popup-blocking')
+    # options.add_argument('--disable-translate')
+    # puts "hahahahah"
+
+    # driver = Selenium::WebDriver.for :chrome, options: options
+
+    Selenium::WebDriver::Chrome.driver_path="/home/fen/Documents/script_for_stock/chromedriver"
+    driver = Selenium::WebDriver.for :chrome
+    driver.get("http://google.com")
+    
+    # driver = Selenium::WebDriver.for :firefox
+    # driver.navigate.to 'https://finance.yahoo.com/'
+    puts driver.title
+    sleep 10
+    headless.destroy
+  end
+
+  def xin_zi_yuan
+    # fill in stock_abb_array
+    if @db.collection_names.size == 0
+      crawl_stock_name
+    else
+      @stock_abb_array = @client[:stocks].find({}).map{|e| e["stock_name"]}
+    end
+
+    data = @yahoo_client.quotes(@stock_abb_array, [:last_trade_date, :last_trade_price, :change_in_percent])
+    data.each { |e|
+      puts e.change_in_percent, e.symbol
+    }
+  end
+
+
+
+  def loop_stock_array
+    if @db.collection_names.size == 0
+      crawl_stock_name
+    else
+      @stock_abb_array = @client[:stocks].find({}).map{|e| e["stock_name"]}
+    end
+    @stock_abb_array.each_slice(100) {|su_arr|
+    # @stock_abb_array.each {|su_arr|
+      # http://wern-ancheta.com/blog/2015/04/05/getting-started-with-the-yahoo-finance-api/
+      # s: Symbol, a: Ask, b: Bid, b2: Ask (Realtime), b3: Bid (Realtime), k: 52 Week High, j: 52 week Low, 
+      # j6: Percent Change From 52 week Low, k5: Percent Change From 52 week High, v: Volume, j1: Market Capitalization
+      # full_url = "http://finance.yahoo.com/d/quotes.csv?s=#{sub_url}&f=sabb2b3jkj6k5vj1"
+
+      # c1 – change
+      # c – change & percentage change
+      # c6 – change (realtime)
+      # k2 – change percent
+      # p2 – change in percent
+      # d1 – last trade date
+      # d2 – trade date
+      # t1 – last trade time
+      # c8 – after hours change
+      # sub_url  = su_arr.join(",")
+      # full_url = "http://finance.google.com/finance/info?client=ig&q=NASDAQ%3A#{sub_url}"
+      # full_url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=#{su_arr}&interval=1min&apikey=4IW1LB9F2Y5ISREC"
+      # get_stockinfo_from_google(full_url)
+      # get_stockinfo_from_alphavantage(full_url)
+      # sleep 0.01
+
+
+      # if ( Time.now.min / 10 ).even?
+        sub_url  = su_arr.join("+")
+        full_url = "http://download.finance.yahoo.com/d/quotes.csv?s=#{sub_url}&f=sc1cc6k2p2d1d2t1c8"
+        get_stockinfo_from_yahoo(full_url)
+      # end
+    }
+  end
+
+  def get_stockinfo_from_yahoo(url)
+    CSV.new(open(url)).each do |line|
+      if line[1] != "N/A"
+        stock_name = line[0].to_s
+        changed_value = line[1].to_f
+        percent_stri  = line[5].to_s
+        iS_increased  = false
+        iS_xiangu     = false
+        if percent_stri =~ /\+/
+          iS_increased = true
+        end
+        percent_num = percent_stri.gsub(/\+|\-|\%/, '').to_f
+        trade_date = line[6] #%d\%m%m\xxxx
+        last_trade_time = line[8] # h:mm pm
+        if iS_increased and percent_num > 20      
+          puts "#{@count}"
+          puts "Name: #{stock_name},  increase?: #{iS_increased},   percent: #{percent_num}, last_time: #{trade_date}-#{last_trade_time}"
+          puts ""
+        end
+        minutes_run = ( Time.now.to_i - Time.parse("#{Time.now.strftime('%Y-%m-%d')} 09:30:00").to_i) / 60
+        collection = @client[:stocks]
+        three_minute_rate = percent_num / minutes_run * 3
+
+# ============== how to define a xian gu ======================
+        if percent_num > 30 and iS_increased
+          iS_xiangu = true
+        elsif ( three_minute_rate ) > 9 and iS_increased
+          iS_xiangu = true
+        end
+# ================================================================
+
+
+        if collection.find( { stock_name:  stock_name } ).first.nil?
+          result = collection.insert_one({ stock_name: stock_name, 
+                                           stock_increased: iS_increased,
+                                           percent_num: percent_num,
+                                           three_minute_rate: three_minute_rate,
+                                           xian_gu: iS_xiangu
+          })
+        else
+          result = collection.update_one( { stock_name: stock_name },
+                                          { '$set': { stock_increased: iS_increased,
+                                                      percent_num: percent_num,
+                                                      three_minute_rate: three_minute_rate,
+                                                      xian_gu: iS_xiangu 
+                                        } } )
+        end
+        @count += 1
+      end
+    end
+  end
+
+  def get_stockinfo_from_google(url)
+    begin
+      respon = open(url)
+      raise "connecton" unless respon.status.first == "200"
+    rescue
+      binding.pry
+      sleep 1
+      retry            
+    end
+    result_arr = JSON.parse(respon.read.gsub(/\n|\/\//, ''))
+    result_arr.each { |e|
+      stock_name    = e["t"]
+      changed_value = e["c"].gsub(/\+|\-|\%/, '').to_f
+      percent_stri  = e["cp"]
+      unless e["ec"].nil?
+        pre_changeval = e["ec"].to_s.gsub(/\+|\-|\%/, '').to_f
+        pre_market_ch = e["ecp"]
+        pre_percent_num = pre_market_ch.gsub(/\+|\-|\%/, '').to_f
+      end
+      iS_increased  = false
+      iS_pre_increase = false
+      iS_xiangu     = false
+      if e["c"] =~ /\+/
+        iS_increased = true
+      end
+      if e["ec"] =~ /\+/
+        iS_pre_increase = true
+      end
+      percent_num = percent_stri.gsub(/\+|\-|\%/, '').to_f
+      # binding.pry if stock_name == "ESES"
+      trade_date = e["lt_dts"] #%d\%m%m\xxxx
+      if ( iS_increased and percent_num > 20 ) or (iS_pre_increase and pre_percent_num > 5 )
+        puts "#{@count}"
+        puts "Name: #{stock_name},  increase?: #{iS_increased}, percent: #{percent_num}, last_time: #{trade_date}, pre-market: #{pre_percent_num}"
+        puts ""
+      end
+      minutes_run = ( Time.now.to_i - Time.parse("#{Time.now.strftime('%Y-%m-%d')} 09:30:00").to_i) / 60
+      collection = @client[:stocks]
+      three_minute_rate = percent_num / minutes_run * 3
+
+  # ============== how to define a xian gu ======================
+      if ( percent_num > 30 and iS_increased ) or ( iS_pre_increase and pre_percent_num > 5 )
+        iS_xiangu = true
+      elsif ( three_minute_rate ) > 9 and iS_increased
+        iS_xiangu = true
+      end
+  # ================================================================
+
+
+      if collection.find( { stock_name:  stock_name } ).first.nil?
+        result = collection.insert_one({ stock_name: stock_name, 
+                                         stock_increased: iS_increased,
+                                         percent_num: percent_num,
+                                         three_minute_rate: three_minute_rate,
+                                         pre_market: pre_percent_num,
+                                         xian_gu: iS_xiangu
+        })
+      else
+        result = collection.update_one( { stock_name: stock_name },
+                                        { '$set': { stock_increased: iS_increased,
+                                                    percent_num: percent_num,
+                                                    three_minute_rate: three_minute_rate,
+                                                    pre_market: pre_percent_num,
+                                                    xian_gu: iS_xiangu 
+                                      } } )
+      end
+      @count += 1
+
+    }
+  end
+
+  def get_stockinfo_from_alphavantage(url)
+    begin
+      # respon = open(url)
+      respon = URI.parse(url).read
+      # raise "connecton" unless respon.status.first == "200"
+    rescue Exception => e
+      puts e.message
+      puts e.backtrace.inspect
+      # binding.pry
+      sleep 1
+      retry            
+    end
+    result_arr = JSON.parse(respon)
+    begin
+    stock_name    = result_arr["Meta Data"]["2. Symbol"]
+      
+    rescue Exception => e
+     return false      
+    end
+    changed_value = result_arr["Time Series (1min)"].first.last["4. close"].to_f
+    open_price    = result_arr["Time Series (1min)"].first.last["1. open"].to_f
+    percent_stri  = ( changed_value - open_price ) / open_price
+    # unless result_arr["ec"].nil?
+    #   pre_changeval = result_arr["ec"].to_s.gsub(/\+|\-|\%/, '').to_f
+    #   pre_market_ch = result_arr["ecp"]
+    #   pre_percent_num = pre_market_ch.gsub(/\+|\-|\%/, '').to_f
+    # end
+    iS_increased  = false
+    # iS_pre_increase = false
+    iS_xiangu     = false
+    if percent_stri > 0
+      iS_increased = true
+    end
+    # if result_arr["ec"] =~ /\+/
+    #   iS_pre_increase = true
+    # end
+    percent_num = percent_stri * 100
+    # binding.pry if stock_name == "ESES"
+    trade_date = result_arr["Meta Data"]["3. Last Refreshed"] #%d\%m%m\xxxx
+    puts percent_num, percent_stri, stock_name
+    if ( iS_increased and percent_num > 20 )
+      puts "#{@count}"
+      puts "Name: #{stock_name},  increase?: #{iS_increased}, percent: #{percent_num}, last_time: #{trade_date}"
+      puts ""
+    end
+    minutes_run = ( Time.now.to_i - Time.parse("#{Time.now.strftime('%Y-%m-%d')} 09:30:00").to_i) / 60
+    collection = @client[:stocks]
+    three_minute_rate = percent_num / minutes_run * 3
+
+# ============== how to define a xian gu ======================
+    if ( percent_num > 30 and iS_increased )
+      iS_xiangu = true
+    elsif ( three_minute_rate ) > 9 and iS_increased
+      iS_xiangu = true
+    end
+# ================================================================
+
+
+    if collection.find( { stock_name:  stock_name } ).first.nil?
+      result = collection.insert_one({ stock_name: stock_name, 
+                                       stock_increased: iS_increased,
+                                       percent_num: percent_num,
+                                       three_minute_rate: three_minute_rate,
+                                       #pre_market: pre_percent_num,
+                                       xian_gu: iS_xiangu
+      })
+    else
+      result = collection.update_one( { stock_name: stock_name },
+                                      { '$set': { stock_increased: iS_increased,
+                                                  percent_num: percent_num,
+                                                  three_minute_rate: three_minute_rate,
+                                                  #pre_market: pre_percent_num,
+                                                  xian_gu: iS_xiangu 
+                                    } } )
+    end
+    @count += 1
+  end
+
+end
+
+puts "===================== >> 30% ================"
+# RealTimeXianStock.new(0.2).loop_stock_array
+RealTimeXianStock.new(0.2).pa_wang_ye
+
+# CrawlStockName.new.get_stockinfo_from_yahoo("http://finance.yahoo.com/d/quotes.csv?s=AAPL+GOOG+NFLX&f=sabb2b3jkj6k5vj1")
+# look at the reference here http://www.jarloo.com/yahoo_finance/
